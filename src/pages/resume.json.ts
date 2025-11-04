2effd3b81057b03494273862e2ad3e3675e2df58
Alberto Castañeiras <alberto@casta.me>
2025-11-04 01:45:49 +0100

    fix: comment @resume

--- src/pages/resume.json.ts
+++ src/pages/resume.json.ts
@@ -1,11 +1,11 @@
 import type { APIRoute } from 'astro';
 
-import resume from '@resume';
+// import resume from '@resume';
 
-export const GET: APIRoute = () =>
-  new Response(JSON.stringify(resume), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-  });
+// export const GET: APIRoute = () =>
+//   new Response(JSON.stringify(resume), {
+//     status: 200,
+//     headers: {
+//       'Content-Type': 'application/json; charset=utf-8',
+//     },
+//   });
